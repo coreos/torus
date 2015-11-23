7fd62bafb64e2e069e94b7bd8a7f2c22948c48f5
Barak Michener <me@barakmich.com>
2015-11-23 07:48:49 -0500

    improve benchmark and test

--- storage/mfile_test.go
+++ storage/mfile_test.go
@@ -3,17 +3,26 @@ package storage
 import (
 	"io/ioutil"
 	"os"
+	"reflect"
 	"testing"
 )
 
-func BenchmarkWrites(b *testing.B) {
-	f, err := ioutil.TempFile(os.TempDir(), "mfiletest")
+func makeTempFilename() (string, error) {
+	f, err := ioutil.TempFile("", "mfiletest")
 	if err != nil {
-		b.Fatal(err)
+		return "", err
 	}
 	n := f.Name()
 	f.Close()
 	os.Remove(n)
+	return n, nil
+}
+
+func benchmarkWrites(b *testing.B, flush bool) {
+	n, err := makeTempFilename()
+	if err != nil {
+		b.Fatal(err)
+	}
 	err = CreateMFile(n, 1<<23) // 8 MB
 	if err != nil {
 		b.Fatal(err)
@@ -26,7 +35,39 @@ func BenchmarkWrites(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		// Cycle through the all the blocks in the file.
 		//   (file size / block size = 8192 blocks = 1<<13)
-		m.WriteBlock(uint64(i%(1<<13)), []byte("some data"))
-		m.Flush()
+		m.WriteBlock(uint64(i%(1<<13)), []byte("some data and more"))
+		if flush {
+			m.Flush()
+		}
+	}
+}
+
+func BenchmarkWrites(b *testing.B) {
+	benchmarkWrites(b, true)
+}
+
+func BenchmarkWritesNoFlush(b *testing.B) {
+	benchmarkWrites(b, false)
+}
+
+func TestWrites(t *testing.T) {
+	n, err := makeTempFilename()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = CreateMFile(n, 1<<23) // 8 MB
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(n)
+	m, err := OpenMFile(n, 1024) // Block Size: 1 KB
+	defer m.Close()
+
+	data := []byte("some data")
+	m.WriteBlock(2, data)
+	m.Flush()
+	b := m.GetBlock(2)
+	if !reflect.DeepEqual(b[:len(data)], data) {
+		t.Fatal("Got useless data back")
 	}
 }
