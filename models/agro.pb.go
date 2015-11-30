65f6a4b360d93bafff5a32af8dc854752deeac38
Barak Michener <me@barakmich.com>
2015-11-30 14:16:41 -0500

    populate type and proto

--- models/agro.pb.go
+++ models/agro.pb.go
@@ -128,9 +128,10 @@ func (m *PeerInfo) String() string { return proto.CompactTextString(m) }
 func (*PeerInfo) ProtoMessage()    {}
 
 type Ring struct {
-	Type  uint32            `protobuf:"varint,1,opt,name=type,proto3" json:"type,omitempty"`
-	UUIDs []string          `protobuf:"bytes,2,rep,name=uuids" json:"uuids,omitempty"`
-	Attrs map[string][]byte `protobuf:"bytes,3,rep,name=attrs" json:"attrs,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+	Type    uint32            `protobuf:"varint,1,opt,name=type,proto3" json:"type,omitempty"`
+	Version uint32            `protobuf:"varint,2,opt,name=version,proto3" json:"version,omitempty"`
+	UUIDs   []string          `protobuf:"bytes,3,rep,name=uuids" json:"uuids,omitempty"`
+	Attrs   map[string][]byte `protobuf:"bytes,4,rep,name=attrs" json:"attrs,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
 }
 
 func (m *Ring) Reset()         { *m = Ring{} }
@@ -434,9 +435,14 @@ func (m *Ring) MarshalTo(data []byte) (int, error) {
 		i++
 		i = encodeVarintAgro(data, i, uint64(m.Type))
 	}
+	if m.Version != 0 {
+		data[i] = 0x10
+		i++
+		i = encodeVarintAgro(data, i, uint64(m.Version))
+	}
 	if len(m.UUIDs) > 0 {
 		for _, s := range m.UUIDs {
-			data[i] = 0x12
+			data[i] = 0x1a
 			i++
 			l = len(s)
 			for l >= 1<<7 {
@@ -456,7 +462,7 @@ func (m *Ring) MarshalTo(data []byte) (int, error) {
 		}
 		github_com_gogo_protobuf_sortkeys.Strings(keysForAttrs)
 		for _, k := range keysForAttrs {
-			data[i] = 0x1a
+			data[i] = 0x22
 			i++
 			v := m.Attrs[k]
 			mapSize := 1 + len(k) + sovAgro(uint64(len(k))) + 1 + len(v) + sovAgro(uint64(len(v)))
@@ -628,6 +634,9 @@ func (m *Ring) Size() (n int) {
 	_ = l
 	if m.Type != 0 {
 		n += 1 + sovAgro(uint64(m.Type))
+	}
+	if m.Version != 0 {
+		n += 1 + sovAgro(uint64(m.Version))
 	}
 	if len(m.UUIDs) > 0 {
 		for _, s := range m.UUIDs {
@@ -1648,6 +1657,25 @@ func (m *Ring) Unmarshal(data []byte) error {
 				}
 			}
 		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Version", wireType)
+			}
+			m.Version = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowAgro
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := data[iNdEx]
+				iNdEx++
+				m.Version |= (uint32(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		case 3:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field UUIDs", wireType)
 			}
@@ -1676,7 +1704,7 @@ func (m *Ring) Unmarshal(data []byte) error {
 			}
 			m.UUIDs = append(m.UUIDs, string(data[iNdEx:postIndex]))
 			iNdEx = postIndex
-		case 3:
+		case 4:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Attrs", wireType)
 			}
--- ring.go
+++ ring.go
@@ -1,6 +1,12 @@
 package agro
 
+type RingType int
+
 type Ring interface {
 	GetBlockPeers(key BlockRef, n int)
 	GetINodePeers(key INodeRef, n int)
+
+	Describe() string
+	Type() RingType
+	Version() int
 }
