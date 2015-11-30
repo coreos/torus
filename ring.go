07c5291357d51d8d82fba4a45d9b8bdfd5d40021
Barak Michener <me@barakmich.com>
2015-11-30 15:58:57 -0500

    add ring functions into the metadata service

--- ring.go
+++ ring.go
@@ -5,8 +5,11 @@ type RingType int
 type Ring interface {
 	GetBlockPeers(key BlockRef, n int) ([]string, error)
 	GetINodePeers(key INodeRef, n int) ([]string, error)
+	Members() []string
 
 	Describe() string
 	Type() RingType
 	Version() int
+
+	Marshal() ([]byte, error)
 }
