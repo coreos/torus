58fd589383675d701073c5cc9c7065201357fbe0
Barak Michener <me@barakmich.com>
2015-11-30 14:31:57 -0500

    add empty ring

--- ring.go
+++ ring.go
@@ -3,8 +3,8 @@ package agro
 type RingType int
 
 type Ring interface {
-	GetBlockPeers(key BlockRef, n int)
-	GetINodePeers(key INodeRef, n int)
+	GetBlockPeers(key BlockRef, n int) ([]string, error)
+	GetINodePeers(key INodeRef, n int) ([]string, error)
 
 	Describe() string
 	Type() RingType
