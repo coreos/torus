d9c426ced35c686924e2f41dfc2f03e49f5dccb8
Barak Michener <me@barakmich.com>
2015-11-30 19:34:03 -0500

    implement stubs to check other locations

--- server/distributor_client.go
+++ server/distributor_client.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"errors"
 	"time"
 
 	"github.com/barakmich/agro"
@@ -13,23 +14,49 @@ const (
 	clientTimeout = 500 * time.Millisecond
 )
 
+// TODO(barakmich): Clean up errors
+
 type distClient struct {
 	dist *distributor
-	//TODO(barakmich): Connection pooling/keep-alive
+	//TODO(barakmich): Better connection pooling
+	openConns map[string]*grpc.ClientConn
 }
 
 func newDistClient(d *distributor) *distClient {
 	return &distClient{
-		dist: d,
+		dist:      d,
+		openConns: make(map[string]*grpc.ClientConn),
 	}
 }
 
-func (d *distClient) GetBlock(ctx context.Context, addr string, b agro.BlockRef) ([]byte, error) {
-	conn, err := grpc.Dial(addr, grpc.WithInsecure())
-	if err != nil {
-		return nil, err
+func (d *distClient) getConn(uuid string) *grpc.ClientConn {
+	if conn, ok := d.openConns[uuid]; ok {
+		return conn
 	}
-	defer conn.Close()
+	pi := d.dist.srv.peersMap[uuid]
+	conn, err := grpc.Dial(pi.Address, grpc.WithInsecure())
+	if err != nil {
+		return nil
+	}
+	d.openConns[uuid] = conn
+	return conn
+}
+
+func (d *distClient) Close() error {
+	for _, c := range d.openConns {
+		err := c.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (d *distClient) GetBlock(ctx context.Context, uuid string, b agro.BlockRef) ([]byte, error) {
+	conn := d.getConn(uuid)
+	if conn == nil {
+		return nil, agro.ErrBlockUnavailable
+	}
 	client := models.NewAgroStorageClient(conn)
 	br := &models.BlockRef{
 		Volume: uint64(b.Volume),
@@ -53,12 +80,11 @@ func (d *distClient) GetBlock(ctx context.Context, addr string, b agro.BlockRef)
 	return resp.Blocks[0].Data, nil
 }
 
-func (d *distClient) GetINode(ctx context.Context, addr string, b agro.INodeRef) (*models.INode, error) {
-	conn, err := grpc.Dial(addr, grpc.WithInsecure())
-	if err != nil {
-		return nil, err
+func (d *distClient) GetINode(ctx context.Context, uuid string, b agro.INodeRef) (*models.INode, error) {
+	conn := d.getConn(uuid)
+	if conn == nil {
+		return nil, agro.ErrINodeUnavailable
 	}
-	defer conn.Close()
 	client := models.NewAgroStorageClient(conn)
 	ref := &models.INodeRef{
 		Volume: uint64(b.Volume),
@@ -79,4 +105,61 @@ func (d *distClient) GetINode(ctx context.Context, addr string, b agro.INodeRef)
 		return nil, agro.ErrINodeUnavailable
 	}
 	return resp.INodes[0], nil
+}
+
+func (d *distClient) PutINode(ctx context.Context, uuid string, b agro.INodeRef, inode *models.INode, rep int) error {
+	conn := d.getConn(uuid)
+	if conn == nil {
+		return agro.ErrINodeUnavailable
+	}
+	client := models.NewAgroStorageClient(conn)
+	ref := &models.INodeRef{
+		Volume: uint64(b.Volume),
+		INode:  uint64(b.INode),
+	}
+	newctx, cancel := context.WithTimeout(ctx, clientTimeout)
+	defer cancel()
+	resp, err := client.PutINode(newctx, &models.PutINodeRequest{
+		Refs:   []*models.INodeRef{ref},
+		INodes: []*models.INode{inode},
+	})
+	if err != nil {
+		if err == context.DeadlineExceeded {
+			return agro.ErrINodeUnavailable
+		}
+		return err
+	}
+	if !resp.Ok {
+		return errors.New(resp.Err)
+	}
+	return nil
+}
+
+func (d *distClient) PutBlock(ctx context.Context, uuid string, b agro.BlockRef, data []byte, rep int) error {
+	conn := d.getConn(uuid)
+	if conn == nil {
+		return agro.ErrBlockUnavailable
+	}
+	client := models.NewAgroStorageClient(conn)
+	ref := &models.BlockRef{
+		Volume: uint64(b.Volume),
+		INode:  uint64(b.INode),
+		Block:  uint64(b.Index),
+	}
+	newctx, cancel := context.WithTimeout(ctx, clientTimeout)
+	defer cancel()
+	resp, err := client.PutBlock(newctx, &models.PutBlockRequest{
+		Refs:   []*models.BlockRef{ref},
+		Blocks: [][]byte{data},
+	})
+	if err != nil {
+		if err == context.DeadlineExceeded {
+			return agro.ErrBlockUnavailable
+		}
+		return err
+	}
+	if !resp.Ok {
+		return errors.New(resp.Err)
+	}
+	return nil
 }
--- server/distributor_rpc.go
+++ server/distributor_rpc.go
@@ -1,16 +1,53 @@
 package server
 
 import (
+	"github.com/barakmich/agro"
 	"github.com/barakmich/agro/models"
 	"golang.org/x/net/context"
 )
 
 func (d *distributor) Block(ctx context.Context, req *models.BlockRequest) (*models.BlockResponse, error) {
-	return nil, nil
+	var out *models.BlockResponse
+	for _, b := range req.Blockrefs {
+		ref := agro.BlockRef{
+			INodeRef: agro.INodeRef{
+				Volume: agro.VolumeID(b.Volume),
+				INode:  agro.INodeID(b.INode),
+			},
+			Index: agro.IndexID(b.Block),
+		}
+		data, err := d.blocks.GetBlock(ctx, ref)
+		if err != nil {
+			clog.Warningf("remote asking for non-existent block")
+			out.Blocks = append(out.Blocks, &models.Block{
+				Ok: false,
+			})
+			continue
+		}
+		out.Blocks = append(out.Blocks, &models.Block{
+			Ok:   true,
+			Data: data,
+		})
+	}
+	return out, nil
 }
 
 func (d *distributor) INode(ctx context.Context, req *models.INodeRequest) (*models.INodeResponse, error) {
-	return nil, nil
+	var out *models.INodeResponse
+	for _, b := range req.Inoderefs {
+		ref := agro.INodeRef{
+			Volume: agro.VolumeID(b.Volume),
+			INode:  agro.INodeID(b.INode),
+		}
+		in, err := d.inodes.GetINode(ctx, ref)
+		if err != nil {
+			clog.Warningf("remote asking for non-existent inode")
+			out.INodes = append(out.INodes, &models.INode{})
+			continue
+		}
+		out.INodes = append(out.INodes, in)
+	}
+	return out, nil
 }
 
 func (d *distributor) PutBlock(ctx context.Context, req *models.PutBlockRequest) (*models.PutResponse, error) {
