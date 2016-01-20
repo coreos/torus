b0d5b4f9af5e729922ced50ef168230263ffa7fb
Barak Michener <me@barakmich.com>
2016-01-20 15:48:37 -0500

    add monitoring to distributor

--- server/distributor_rpc.go
+++ server/distributor_rpc.go
@@ -9,7 +9,9 @@ import (
 )
 
 func (d *distributor) Block(ctx context.Context, req *models.BlockRequest) (*models.BlockResponse, error) {
+	promDistBlockRPCs.Inc()
 	out := &models.BlockResponse{}
+	fail := false
 	for _, b := range req.BlockRefs {
 		ref := agro.BlockRef{
 			INodeRef: agro.INodeRef{
@@ -24,6 +26,7 @@ func (d *distributor) Block(ctx context.Context, req *models.BlockRequest) (*mod
 			out.Blocks = append(out.Blocks, &models.Block{
 				Ok: false,
 			})
+			fail = true
 			continue
 		}
 		out.Blocks = append(out.Blocks, &models.Block{
@@ -31,11 +34,16 @@ func (d *distributor) Block(ctx context.Context, req *models.BlockRequest) (*mod
 			Data: data,
 		})
 	}
+	if fail {
+		promDistBlockRPCFailures.Inc()
+	}
 	return out, nil
 }
 
 func (d *distributor) INode(ctx context.Context, req *models.INodeRequest) (*models.INodeResponse, error) {
+	promDistINodeRPCs.Inc()
 	out := &models.INodeResponse{}
+	fail := false
 	for _, b := range req.INodeRefs {
 		ref := agro.INodeRef{
 			Volume: agro.VolumeID(b.Volume),
@@ -45,14 +53,21 @@ func (d *distributor) INode(ctx context.Context, req *models.INodeRequest) (*mod
 		if err != nil {
 			clog.Warningf("remote asking for non-existent inode")
 			out.INodes = append(out.INodes, &models.INode{})
+			fail = true
 			continue
 		}
 		out.INodes = append(out.INodes, in)
+	}
+	if fail {
+		promDistINodeRPCFailures.Inc()
 	}
 	return out, nil
 }
 
 func (d *distributor) PutBlock(ctx context.Context, req *models.PutBlockRequest) (*models.PutResponse, error) {
+	d.mut.RLock()
+	defer d.mut.RUnlock()
+	promDistPutBlockRPCs.Inc()
 	if len(req.Refs) != len(req.Blocks) {
 		return &models.PutResponse{Err: "malformed request"}, nil
 	}
@@ -66,6 +81,7 @@ func (d *distributor) PutBlock(ctx context.Context, req *models.PutBlockRequest)
 		}
 		peers, err := d.ring.GetBlockPeers(ref)
 		if err != nil {
+			promDistPutBlockRPCFailures.Inc()
 			return nil, err
 		}
 		ok := false
@@ -77,6 +93,7 @@ func (d *distributor) PutBlock(ctx context.Context, req *models.PutBlockRequest)
 		}
 		if !ok {
 			clog.Warningf("trying to write block that doesn't belong to me. aborting.")
+			promDistPutBlockRPCFailures.Inc()
 			return &models.PutResponse{Err: "not my block"}, nil
 		}
 		d.blocks.WriteBlock(ctx, ref, req.Blocks[i])
@@ -89,6 +106,7 @@ func (d *distributor) PutBlock(ctx context.Context, req *models.PutBlockRequest)
 func (d *distributor) PutINode(ctx context.Context, req *models.PutINodeRequest) (*models.PutResponse, error) {
 	d.mut.RLock()
 	defer d.mut.RUnlock()
+	promDistPutINodeRPCs.Inc()
 	if len(req.Refs) != len(req.INodes) {
 		return &models.PutResponse{Err: "malformed request"}, nil
 	}
@@ -99,6 +117,7 @@ func (d *distributor) PutINode(ctx context.Context, req *models.PutINodeRequest)
 		}
 		peers, err := d.ring.GetINodePeers(ref)
 		if err != nil {
+			promDistPutINodeRPCFailures.Inc()
 			return nil, err
 		}
 		ok := false
@@ -110,6 +129,7 @@ func (d *distributor) PutINode(ctx context.Context, req *models.PutINodeRequest)
 		}
 		if !ok {
 			clog.Warningf("trying to write inode that doesn't belong to me. aborting.")
+			promDistPutINodeRPCFailures.Inc()
 			return &models.PutResponse{Err: "not my inode"}, nil
 		}
 		d.inodes.WriteINode(ctx, ref, req.INodes[i])
@@ -122,8 +142,14 @@ func (d *distributor) PutINode(ctx context.Context, req *models.PutINodeRequest)
 func (d *distributor) RebalanceMessage(ctx context.Context, req *models.RebalanceRequest) (*models.RebalanceResponse, error) {
 	d.mut.RLock()
 	defer d.mut.RUnlock()
+	promDistRebalanceRPCs.Inc()
 	if d.rebalancer == nil {
+		promDistRebalanceRPCFailures.Inc()
 		return nil, errors.New("not rebalancing")
 	}
-	return d.rebalancer.RebalanceMessage(ctx, req)
+	resp, err := d.rebalancer.RebalanceMessage(ctx, req)
+	if err != nil {
+		promDistRebalanceRPCFailures.Inc()
+	}
+	return resp, err
 }
