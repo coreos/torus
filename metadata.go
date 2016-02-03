6d98edfab82557279be890dbcb272bf4106d249d
Barak Michener <me@barakmich.com>
2016-02-02 23:46:46 -0500

    remove RebalanceStatus and the weird mds stuff surrounding it

--- metadata.go
+++ metadata.go
@@ -125,11 +125,8 @@ type MetadataService interface {
 	// TODO(barakmich): THESE NEED A LEASE ID
 	RegisterPeer(*models.PeerInfo) error
 	ClaimVolumeINodes(volume string, inodes *roaring.RoaringBitmap) error
-	OpenRebalanceChannels() (inOut [2]chan *models.RebalanceStatus, leader bool, err error)
 	// ^^^^^^^
 
-	SetRebalanceSnapshot(uint64, []byte) error
-	GetRebalanceSnapshot() (uint64, []byte, error)
 	ModifyDeadMap(volume string, live *roaring.RoaringBitmap, dead *roaring.RoaringBitmap) error
 	GetVolumeLiveness(volume string) (*roaring.RoaringBitmap, []*roaring.RoaringBitmap, error)
 
