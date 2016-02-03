0a387e544a8d0aa826ba0a7b642e65a34ad02e97
Barak Michener <me@barakmich.com>
2016-02-02 21:53:25 -0500

    no, really, remove rebalancer. Always replace the ring.

--- server/distributor_rebalance.go
+++ server/distributor_rebalance.go
@@ -4,32 +4,10 @@ import (
 	"time"
 
 	"github.com/coreos/agro"
-	"github.com/coreos/agro/models"
-	"github.com/coreos/agro/ring"
-	"github.com/coreos/pkg/capnslog"
 )
-
-type RebalanceStrategy int32
-
-type Rebalancer interface {
-	Leader(chans [2]chan *models.RebalanceStatus)
-	AdvanceState(s *models.RebalanceStatus) (*models.RebalanceStatus, bool, error)
-	OnError(error) *models.RebalanceStatus
-	Timeout()
-}
-
-const (
-	Error   RebalanceStrategy = iota
-	Replace                   = 1
-	Full                      = 2
-)
-
-type makeRebalanceFunc func(d *distributor, newring agro.Ring) Rebalancer
 
 var (
-	rebalanceTimeout   = 30 * time.Second
-	rebalancerRegistry = make(map[RebalanceStrategy]makeRebalanceFunc)
-	rlog               = capnslog.NewPackageLogger("github.com/coreos/agro", "rebalancer")
+	rebalanceTimeout = 30 * time.Second
 )
 
 // Goroutine which watches for new rings and kicks off
@@ -53,118 +31,12 @@ exit:
 				if newring.Version() != d.ring.Version()+1 {
 					panic("replacing old ring with ring in the far future!")
 				}
-				d.Rebalance(newring)
+				d.mut.Lock()
+				d.ring = newring
+				d.mut.Unlock()
 			} else {
 				break exit
 			}
 		}
 	}
-}
-
-func (d *distributor) Rebalance(newring agro.Ring) {
-	d.srv.updatePeerMap()
-	isMember := d.ring.Members().Union(newring.Members()).Has(d.UUID())
-	if !isMember {
-		clog.Infof("rebalance detected, but not a member")
-	}
-	// TODO(barakmich): Rebalancing is tricky. But here's the entry point.
-	clog.Infof("rebalancing beginning: new ring version %d for %s", newring.Version(), d.UUID())
-	chans, leader, err := d.srv.mds.OpenRebalanceChannels()
-	if err != nil {
-		clog.Error(err)
-		return
-	}
-	if leader {
-		clog.Infof("elected as leader: %s", d.UUID())
-		d.rebalanceLeader(chans, newring)
-	} else {
-		clog.Infof("elected to follow")
-		d.rebalanceFollower(chans, newring)
-	}
-	d.mut.Lock()
-	defer d.mut.Unlock()
-	d.rebalancer = nil
-}
-
-func (d *distributor) rebalanceLeader(chans [2]chan *models.RebalanceStatus, newring agro.Ring) {
-	var re Rebalancer
-	switch d.ring.Type() {
-	case ring.Empty:
-		// We can always replace the empty ring.
-		clog.Infof("replacing empty ring")
-		re = rebalancerRegistry[Replace](d, newring)
-	default:
-		re = rebalancerRegistry[Full](d, newring)
-	}
-	d.mut.Lock()
-	d.rebalancer = re
-	d.mut.Unlock()
-	re.Leader(chans)
-	d.srv.mut.Lock()
-	defer d.srv.mut.Unlock()
-	clog.Info("leader: success, setting new ring")
-	d.ring = newring
-	d.srv.mds.SetRing(newring, true)
-	close(chans[1])
-}
-
-func (d *distributor) rebalanceFollower(inOut [2]chan *models.RebalanceStatus, newring agro.Ring) {
-	in, out := inOut[0], inOut[1]
-	for {
-		s := <-in
-		if !s.FromLeader {
-			panic("got a message not from leader")
-		}
-		if d.rebalancer == nil {
-			d.mut.Lock()
-			rlog.Debugf("creating rebalancer %d", s.RebalanceType)
-			d.rebalancer = rebalancerRegistry[RebalanceStrategy(s.RebalanceType)](d, newring)
-			d.mut.Unlock()
-		}
-		news, done, err := d.rebalancer.AdvanceState(s)
-		if err != nil {
-			clog.Error(err)
-			stat := d.rebalancer.OnError(err)
-			if stat != nil {
-				out <- stat
-			}
-			close(out)
-			return
-		}
-		news.UUID = d.UUID()
-		out <- news
-		if done {
-			close(out)
-			d.srv.mut.Lock()
-			defer d.srv.mut.Unlock()
-			clog.Info("follower: success, setting new ring")
-			d.ring = newring
-			return
-		}
-	}
-}
-
-func waitAll(c chan *models.RebalanceStatus, peerList agro.PeerList, phase int32) error {
-	for len(peerList) > 0 {
-
-		// TODO(barakmich) Check if the status is an error, such as the TTL of
-		// the key being lost in etcd (thus a machine has timed out and we're in
-		// trouble). LEASES.
-		stat, ok := <-c
-		if !ok {
-			clog.Error("close before end of rebalance")
-			return agro.ErrClosed
-		}
-		if stat.Phase == phase {
-			for i, m := range peerList {
-				if m == stat.UUID {
-					clog.Debugf("got response from %s", stat.UUID)
-					peerList = append(peerList[:i], peerList[i+1:]...)
-					break
-				}
-			}
-		}
-	}
-	clog.Debugf("finished waiting for members")
-	return nil
 }
