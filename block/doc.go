0f190dab13e466ea65aed9e9377fd999f4416d57
Barak Michener <me@barakmich.com>
2016-06-14 14:05:59 -0700

    Add short package comments to every package. Fixes #194

---
+++ block/doc.go
@@ -0,0 +1,2 @@
+// block provides the implementation of the "block" volume type, using a Torus file as a block device.
+package block
--- version.go
+++ version.go
@@ -1,3 +1,6 @@
+// Torus is a distributed storage system, allowing for the creation and
+// coordination of sharded, replicated files. For more details, see the README
+// at https://github.com/coreos/torus
 package torus
 
 // Version is set by build scripts, do not touch.
