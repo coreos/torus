6be6503cb56fe7ecb8bbb09c2ca07b1408035ebb
Blake Howell <blake@evertrue.com>
2016-06-15 18:04:04 -0400

    Add volume list and delete for torusblk. Fixes #206

--- cmd/torusblk/volume.go
+++ cmd/torusblk/volume.go
@@ -6,8 +6,11 @@ import (
 	"github.com/coreos/torus"
 	"github.com/coreos/torus/block"
 	"github.com/dustin/go-humanize"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
+
+var outputAsCSV bool
 
 var volumeCommand = &cobra.Command{
 	Use:   "volume",
@@ -22,8 +25,23 @@ var volumeCreateCommand = &cobra.Command{
 	Run:   volumeCreateAction,
 }
 
+var volumeDeleteCommand = &cobra.Command{
+	Use:   "delete",
+	Short: "delete a volume in the cluster",
+	Run:   volumeDeleteAction,
+}
+
+var volumeListCommand = &cobra.Command{
+	Use:  	"list",
+	Short: 	"list volumes in the cluster",
+	Run:	volumeListAction,
+}
+
 func init() {
 	volumeCommand.AddCommand(volumeCreateCommand)
+	volumeCommand.AddCommand(volumeListCommand)
+	volumeCommand.AddCommand(volumeDeleteCommand)
+	volumeListCommand.Flags().BoolVarP(&outputAsCSV, "csv", "", false, "output as csv instead")
 }
 
 func volumeAction(cmd *cobra.Command, args []string) {
@@ -44,6 +62,57 @@ func volumeCreateAction(cmd *cobra.Command, args []string) {
 	err = block.CreateBlockVolume(mds, args[0], size)
 	if err != nil {
 		die("error creating volume %s: %v", args[0], err)
+	}
+}
+
+func volumeListAction(cmd *cobra.Command, args []string){
+	if len(args) != 0 {
+		cmd.Usage()
+		os.Exit(1)
+	}
+	mds := mustConnectToMDS()
+	vols, _, err := mds.GetVolumes()
+	if err != nil {
+		die("error listing volumes: %v\n", err)
+	}
+	table := tablewriter.NewWriter(os.Stdout)
+	if outputAsCSV {
+		table.SetBorder(false)
+		table.SetColumnSeparator(",")
+	} else {
+		table.SetHeader([]string{"Volume Name", "Size"})
+	}
+	for _, x := range vols {
+		if x.Type == "block" {
+
+			table.Append([]string{
+				x.Name,
+				humanize.IBytes(x.MaxBytes),
+			})
+		}
+	}
+	table.Render()
+}
+
+func volumeDeleteAction(cmd *cobra.Command, args []string) {
+	if len(args) != 1 {
+		cmd.Usage()
+		os.Exit(1)
+	}
+	name := args[0]
+	mds := mustConnectToMDS()
+	vol, err := mds.GetVolume(name)
+	if err != nil {
+		die("cannot get volume %s (perhaps it doesn't exist): %v", name, err)
+	}
+	switch vol.Type {
+	case "block":
+		err = block.DeleteBlockVolume(mds, name)
+	default:
+		die("volume is type %s, not blocks", vol.Type)
+	}
+	if err != nil {
+		die("cannot delete volume: %v", err)
 	}
 }
 
