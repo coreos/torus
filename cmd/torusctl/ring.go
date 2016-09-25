7e015e608be688b7503556dd8a7f1d1e3458e26d
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2016-09-25 23:40:22 +0900

    torusctl: change default ring type of manual-change to ketama

--- cmd/torusctl/ring.go
+++ cmd/torusctl/ring.go
@@ -52,7 +52,7 @@ func init() {
 	ringCommand.AddCommand(ringGetCommand)
 	ringChangeCommand.Flags().StringSliceVar(&uuids, "uuids", []string{}, "uuids to incorporate in the ring")
 	ringChangeCommand.Flags().BoolVar(&allUUIDs, "all-peers", false, "use all peers in the ring")
-	ringChangeCommand.Flags().StringVar(&ringType, "type", "single", "type of ring to create (empty, single, mod or ketama)")
+	ringChangeCommand.Flags().StringVar(&ringType, "type", "ketama", "type of ring to create (empty, single, mod or ketama)")
 	ringChangeCommand.Flags().IntVarP(&repFactor, "replication", "r", 2, "number of replicas")
 }
 
